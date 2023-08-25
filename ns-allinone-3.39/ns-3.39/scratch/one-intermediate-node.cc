4d90d34ddb7d91f5253fc1b66719da31ac3cf8ea
Stian Hallerud Sundkvist <stianhsu@uio.no>
2023-08-25 17:28:15 +0200

    Adding ns-3 files

---
+++ ns-allinone-3.39/ns-3.39/scratch/one-intermediate-node.cc
@@ -0,0 +1,68 @@
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("OneIntermediateNode");
+
+int
+main(int argc, char* argv[])
+{
+    CommandLine cmd(__FILE__);
+    cmd.Parse(argc, argv);
+
+    Time::SetResolution(Time::NS);
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("PointToPointNetDevice", LOG_LEVEL_INFO);
+
+    NodeContainer nodes;
+    nodes.Create(3);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NodeContainer ab = NodeContainer(nodes.Get(0), nodes.Get(1));
+    NodeContainer bc = NodeContainer(nodes.Get(1), nodes.Get(2));
+
+    NetDeviceContainer devices1 = pointToPoint.Install(ab);
+    NetDeviceContainer devices2 = pointToPoint.Install(bc);
+
+    InternetStackHelper stack;
+    stack.Install(nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer interfaces1 = address.Assign(devices1);
+
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces2 = address.Assign(devices2);
+
+    UdpEchoServerHelper echoServer(9);
+
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(2));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
+
+    UdpEchoClientHelper echoClient(interfaces2.GetAddress(1), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+    pointToPoint.EnablePcapAll ("one-intermediate-node-2");
+
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
+}
---
+++ ns-allinone-3.39/ns-3.39/scratch/three-intermediate-nodes.cc
@@ -0,0 +1,78 @@
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("OneIntermediateNode");
+
+int
+main(int argc, char* argv[])
+{
+    CommandLine cmd(__FILE__);
+    cmd.Parse(argc, argv);
+
+    Time::SetResolution(Time::NS);
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("PointToPointNetDevice", LOG_LEVEL_INFO);
+
+    NodeContainer nodes;
+    nodes.Create(5);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NodeContainer ab = NodeContainer(nodes.Get(0), nodes.Get(1));
+    NodeContainer bc = NodeContainer(nodes.Get(1), nodes.Get(2));
+    NodeContainer cd = NodeContainer(nodes.Get(2), nodes.Get(3));
+    NodeContainer de = NodeContainer(nodes.Get(3), nodes.Get(4));
+
+    NetDeviceContainer devices1 = pointToPoint.Install(ab);
+    NetDeviceContainer devices2 = pointToPoint.Install(bc);
+    NetDeviceContainer devices3= pointToPoint.Install(cd);
+    NetDeviceContainer devices4 = pointToPoint.Install(de);
+
+    InternetStackHelper stack;
+    stack.Install(nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer interfaces1 = address.Assign(devices1);
+
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces2 = address.Assign(devices2);
+
+    address.SetBase("10.1.3.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces3 = address.Assign(devices3);
+
+    address.SetBase("10.1.4.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces4 = address.Assign(devices4);
+
+    UdpEchoServerHelper echoServer(9);
+
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(4));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
+
+    UdpEchoClientHelper echoClient(interfaces4.GetAddress(1), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+    pointToPoint.EnablePcapAll("three-intermediate-nodes");
+
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
+}
