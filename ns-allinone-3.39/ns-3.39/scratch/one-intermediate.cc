f63f7ca3fc5f98ef6f110338ac9e2a30d8dfa485
Stian Hallerud Sundkvist <stianhsu@uio.no>
2023-10-19 13:09:57 +0200

    Adding simulation round to pcap files

--- ns-allinone-3.39/ns-3.39/scratch/one-intermediate.cc
+++ ns-allinone-3.39/ns-3.39/scratch/one-intermediate.cc
@@ -29,7 +29,8 @@ int main(int argc, char* argv[]) {
                       std::atoi(argv[3]),
                       std::atoi(argv[4]));
 
-    int ttl = std::atoi(argv[5]);
+    int simulation_round = std::atoi(argv[5]);
+
     std::ostringstream oss;
     oss << p_info.trailing_s << "-" << p_info.cross_traffic_s << "-" << p_info.link_cap << "-queue-size.csv";
     std::string queue_file = oss.str();
@@ -141,7 +142,7 @@ int main(int argc, char* argv[]) {
     crossTraffic4->Connect(InetSocketAddress(interfaces2.GetAddress(1), 9));
 
     std::ostringstream oss2;
-    oss2 << p_info.trailing_s+30 << "-" << p_info.cross_traffic_s << "-" << p_info.link_cap << "-one-intermediate";
+    oss2 << p_info.trailing_s+30 << "-" << p_info.cross_traffic_s << "-" << p_info.link_cap << "-" << simulation_round << "-one-intermediate";
     std::string pcap_file = oss2.str();
     pointToPoint.EnablePcap(pcap_file, devices2.Get(0));
 
